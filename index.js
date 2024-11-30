42db1126554e92b849df608844d2c04c1c64e401
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2024-11-30 21:14:52 +0200

    las

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-// import studentsRouter from './routers/students.js'
+import studentsRouter from './routers/students.js'
 import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3001;
@@ -9,7 +9,7 @@ app.use(express.json())
 mongoose.connect('mongodb+srv://sdeka2841:RlJWCZvV4wJxlpOq@cluster0.x80oh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('Connected!'));
 
-// app.use('/students', studentsRouter)
+app.use('/students', studentsRouter)
 app.get('/', (req, res) => {
     res.send("welcomsds")
 })
