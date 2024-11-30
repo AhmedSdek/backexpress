137486cbeef46d5f00223a17d97916f66efcdbb0
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2024-11-30 21:18:40 +0200

    v

--- routers/students.js
+++ routers/students.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const students = await studentModel.find();
+        console.log(students)
         res.status(200).json(students)
     } catch (err) {
         res.status(500).send('something wrong')
